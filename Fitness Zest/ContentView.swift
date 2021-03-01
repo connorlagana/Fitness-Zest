65f3814e51793aaf8f872a030a1d0e35c09d99d2
Connor Lagana <connor.lagana@gmail.com>
2021-03-01 15:26:12 -0500

    Initial Commit

---
+++ Fitness Zest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fitness Zest
+//
+//  Created by Connor Lagana on 3/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fitness Zest/Fitness_ZestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_ZestApp.swift
+//  Fitness Zest
+//
+//  Created by Connor Lagana on 3/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_ZestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
